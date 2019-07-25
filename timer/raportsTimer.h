1d1aa7220fd7a0c185a84ff460d7fe6d7dae054c
Woocheck <woocheck@op.pl>
2019-07-25 20:36:52 +0200

    Timer class.

---
+++ timer/raportsTimer.h
@@ -0,0 +1,24 @@
+#ifndef __RAPORTS_TIMER_H__
+#define __RAPORTS_TIMER_H__
+
+#include <ctime>
+
+class RaportsTimer
+{
+    std::time_t time {};
+
+    public:
+        RaportsTimer(){};
+
+        void setTime()
+        {
+            std::localtime( &time );
+        }
+
+        std::time_t getTime()
+        {
+            return time;
+        }
+};
+
+#endif
