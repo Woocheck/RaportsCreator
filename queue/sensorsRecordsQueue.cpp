37c0a45d644a63c046b92eacf158f76d28e70240
Woocheck <woocheck@op.pl>
2019-07-29 23:05:29 +0200

    Old version, I need to change it to chrono library.

---
+++ queue/sensorsRecordsQueue.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <map>
+
+#include "./sensorsRecordsQueue.h"
+#include "../timer/raportsTimer.h"
+
+
+void SingleRecord::setRecordDate(const std::time_t& date )
+{
+    dateOfRecord = date;
+};
+bool SingleRecord::isMeasurmentHaveCorrectDate( const SingleMeasurment& measurment )
+{
+    auto localMeasurment = *std::localtime( &measurment );
+    localMeasurment.tm_sec = 0;
+    auto m = 
+    
+    if( difftime(  , localMeasurment ) == 0 )
+};
+bool SingleRecord::isSetOfMeasurmentsCompleted();
+void SingleRecord::getSingleMeasurment( const SingleMeasurment& );
+void SingleRecord::makeSingleRecord( const std::time_t& );
+std::string SingleRecord::getRecord( const std::time_t& );
--- queue/sensorsRecordsQueue.h
+++ queue/sensorsRecordsQueue.h
@@ -15,7 +15,30 @@ struct SingleMeasurment
 
 class SingleRecord
 {
-    std::map< int, SingleMeasurment > setOfMeasurments;
+    std::map< int, SingleMeasurment > setOfMeasurments {};
+    int numberOfMeasurmentPoints {};
+    SingleMeasurment& measurmentsList;
+    std::time_t dateOfRecord {};
+    std::tm localDateOfRecord{};
+
+    std::string singleRecord;
+
+    public:
+        SingleRecord( int points, SingleMeasurment& list ):
+            numberOfMeasurmentPoints( points ),
+            measurmentsList( list ) 
+            {
+                localDateOfRecord = *std::localtime( &dateOfRecord );
+                localDateOfRecord.tm_sec = 0;
+            };
+
+        void setRecordDate(const std::time_t& );
+        bool isMeasurmentHaveCorrectDate( const SingleMeasurment& measurment );
+        bool isSetOfMeasurmentsCompleted();
+        void getSingleMeasurment( const SingleMeasurment& );
+        void makeSingleRecord( const std::time_t& );
+        std::string getRecord( const std::time_t& );
+        
 
 };
 #endif 
