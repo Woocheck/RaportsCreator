fe6462e56165adf32d852a31b11e8b7eb7330f79
Woocheck <woocheck@op.pl>
2019-07-30 22:17:38 +0200

    MakeTestList implemented.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
 #include <filesystem>
+#include <vector>
+#include<algorithm>
+
+#include "./timer/raportsTimer.h"
+#include "./queue/sensorsRecordsQueue.h"
+
+std::vector<SingleMeasurment> makeTestList( int numberElements );
 
 
 int main()
 {
-    std::filesystem::path path { "testdir/test.txt" };
+    std::vector<SingleMeasurment> testList {};
+    RaportsTimer first {};
+    first.setTime();
+    testList = makeTestList( 100 );
+    RaportsTimer second {};
+    second.setTime();
 
-    std::cout   << "Current path: " << std::filesystem::current_path()
-                << "\nabsolute path: " << std::filesystem::absolute( path )
-                << "\nsystem cmplete: " << std::filesystem::canonical( path )
-                << "\n";
-
+    SingleRecord record( 5, testList );
     
 }
+
+std::vector<SingleMeasurment> makeTestList( int numberElements )
+{
+    std::vector< int > keys { 1, 2, 3, 4, 5 };
+
+    std::vector<SingleMeasurment> list{};
+    for( auto i{0}; i < numberElements; i++ )
+    {
+        std::for_each( keys.begin(), keys.end(),
+            [ &list ](int i)
+            {
+                RaportsTimer time {};
+                time.setTime();
+
+                SingleMeasurment measurment;
+                measurment.sensorKeyNumber = i;
+                measurment.measurementDate = time;
+                measurment.measurmentValue = std::rand()%100000000; 
+                list.push_back( measurment );       
+            });
+    }
+};
