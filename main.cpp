46adefd688f6acc6eb9037ead37d49a7e3a2a15e
Woocheck <woocheck@op.pl>
2019-08-02 10:14:47 +0200

    .

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "./timer/raportsTimer.h"
 #include "./queue/sensorsRecordsQueue.h"
 
-std::vector<SingleMeasurment> makeTestList( int numberElements );
+
 
 
 int main()
@@ -22,24 +22,3 @@ int main()
     
 }
 
-std::vector<SingleMeasurment> makeTestList( int numberElements )
-{
-    std::vector< int > keys { 1, 2, 3, 4, 5 };
-
-    std::vector<SingleMeasurment> list{};
-    for( auto i{0}; i < numberElements; i++ )
-    {
-        std::for_each( keys.begin(), keys.end(),
-            [ &list ](int i)
-            {
-                RaportsTimer time {};
-                time.setTime();
-
-                SingleMeasurment measurment;
-                measurment.sensorKeyNumber = i;
-                measurment.measurementDate = time;
-                measurment.measurmentValue = std::rand()%100000000; 
-                list.push_back( measurment );       
-            });
-    }
-};
